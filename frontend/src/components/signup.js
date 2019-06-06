1d807e395f4aa84ca4f868b146fac66e8528f64c
Tiffany Chong <sciffany@gmail.com>
2019-06-06 23:57:38 +0800

    Add sockets and server

---
+++ frontend/src/components/signup.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Signup extends Component {
+    state = {  }
+    render() { 
+        return ( <div>Signups {this.props.color}</div> );
+    }
+}
+ 
+export default Signup;
