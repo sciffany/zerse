c49acc360f67208f9028676513db7dc2d247a3af
Tiffany Chong <sciffany@gmail.com>
2019-10-24 10:31:54 +0800

    Remove socket listening

--- src/password/password-server.ts
+++ src/password/password-server.ts
@@ -45,54 +45,54 @@ export class PasswordServer {
     this.server.listen(PasswordServer.PORT, () =>
       console.log(`Listening on port ${PasswordServer.PORT}`)
     )
-    this.passwordio.on("connection", socket => {
-      var roomName
-      var userId
-      socket.on("sign up", details => {
-        let roomToJoin: Room = this.roomList.find(
-          room => room.name === details.room
-        )
+    // this.passwordio.on("connection", socket => {
+    //   var roomName
+    //   var userId
+    //   socket.on("sign up", details => {
+    //     let roomToJoin: Room = this.roomList.find(
+    //       room => room.name === details.room
+    //     )
 
-        if (!roomToJoin) {
-          roomToJoin = new Room(details.room)
-          this.roomList.push(roomToJoin)
-        }
+    //     if (!roomToJoin) {
+    //       roomToJoin = new Room(details.room)
+    //       this.roomList.push(roomToJoin)
+    //     }
 
-        try {
-          const user = new User(details.name, roomToJoin)
-          userId = user.id
-          console.log(userId)
-        } catch (err) {
-          socket.emit("err", "User with that name already inside")
-        }
+    //     try {
+    //       const user = new User(details.name, roomToJoin)
+    //       userId = user.id
+    //       console.log(userId)
+    //     } catch (err) {
+    //       socket.emit("err", "User with that name already inside")
+    //     }
 
-        socket.join(details.room)
-        this.passwordio.to(details.room).emit("person joined", {
-          listOfNames: roomToJoin.getWatchers(),
-          playerOrder: roomToJoin.getPlayerOrder()
-        })
-        roomName = details.room
-      })
+    //     socket.join(details.room)
+    //     this.passwordio.to(details.room).emit("person joined", {
+    //       listOfNames: roomToJoin.getWatchers(),
+    //       playerOrder: roomToJoin.getPlayerOrder()
+    //     })
+    //     roomName = details.room
+    //   })
 
-      socket.on("player order", details => {
-        const detail = { code: details.code, name: details.name }
-        this.findRoomByName(roomName).addDetail(detail)
-        this.passwordio.to(roomName).emit("person order", detail)
-      })
+    //   socket.on("player order", details => {
+    //     const detail = { code: details.code, name: details.name }
+    //     this.findRoomByName(roomName).addDetail(detail)
+    //     this.passwordio.to(roomName).emit("person order", detail)
+    //   })
 
-      socket.on("disconnect", () => {
-        if (!roomName) {
-          return
-        }
-        this.findRoomByName(roomName).removeWatcher(userId)
-        this.passwordio
-          .to(roomName)
-          .emit(
-            "user disconnected",
-            this.findRoomByName(roomName).getWatchers()
-          )
-      })
-    })
+    //   socket.on("disconnect", () => {
+    //     if (!roomName) {
+    //       return
+    //     }
+    //     this.findRoomByName(roomName).removeWatcher(userId)
+    //     this.passwordio
+    //       .to(roomName)
+    //       .emit(
+    //         "user disconnected",
+    //         this.findRoomByName(roomName).getWatchers()
+    //       )
+    //   })
+    // })
   }
 
   findRoomByName(roomName): Room {
