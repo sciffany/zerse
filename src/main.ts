98d87509abc2d9a8e9aaa12a1f7e7b5bd90e978d
Tiffany Chong <sciffany@gmail.com>
2022-09-12 11:33:03 +0800

    Add port

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,25 @@ import { PasswordServer } from "./password/passwordServer"
 
 let app = new PasswordServer().getApp()
 export { app }
+module.exports = {
+    apps : [{
+      script: 'index.js',
+      watch: '.'
+    }, {
+      script: './service-worker/',
+      watch: ['./service-worker']
+    }],
+  
+    deploy : {
+      production : {
+        user : 'SSH_USERNAME',
+        host : 'SSH_HOSTMACHINE',
+        ref  : 'origin/master',
+        repo : 'GIT_REPOSITORY',
+        path : 'DESTINATION_PATH',
+        'pre-deploy-local': '',
+        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+        'pre-setup': ''
+      }
+    }
+  };
